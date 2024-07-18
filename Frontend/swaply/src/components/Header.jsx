518ba0883dd2171ce2d12bb5c9913a9747d877a0
AlopezR <adrialopezr@gmail.com>
2024-07-18 19:03:21 +0200

    Header primera Version
    Ya me direis que tal

--- Frontend/swaply/src/components/Header.jsx
+++ Frontend/swaply/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import './Header.css';
 
 const Header = () => {
     return (
         <header>
-            
-            <SearchBar />
+            <h2 className='swaply'>Swaply</h2>
+            <SearchBar className="barra" />
+            <div className='botones'>
+            <button className='iniciar'>Iniciar Sesión</button>
+            <button >Regístrate</button>
+            </div>
         </header>
     );
 };
--- Frontend/swaply/src/components/SearchBar.jsx
+++ Frontend/swaply/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './SearchBar.css'
 const SearchBar = () => {
     return (
         <input type="text" placeholder="Buscar..." />
