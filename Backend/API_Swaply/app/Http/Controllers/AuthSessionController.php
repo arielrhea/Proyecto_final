ba802dc634a050e0ca5bb703a099aa5b3a75ac06
Lucas Naim Vitale <lucasnaimvitale04@gmail.com>
2024-07-22 19:51:31 +0200

    adaptando el login

--- Backend/API_Swaply/app/Http/Controllers/AuthSessionController.php
+++ Backend/API_Swaply/app/Http/Controllers/AuthSessionController.php
@@ -2,21 +2,47 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthSessionController extends Controller
 {
-    public function login(Request $request)
-    {
-        $credentials = $request->only('correoelectronico', 'password');
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $token = $user->createToken('authToken')->plainTextToken;
-            return response()->json(['token' => $token], 200);
+    public function login(Request $request){
+
+        $datos = $request->header();
+        $credenciales['email'] = $datos['email'][0] ?? null;
+        $credenciales['password'] = $datos['password'][0] ?? null;
+
+
+        $reglas = [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+        
+        $validator = Validator::make($credenciales, $reglas);
+
+        if($validator->fails()){
+            $errores = $validator->getMessageBag()->all();
+            return response()->json($errores, 400);
         }
-        return response()->json(['message' => 'Unauthorized'], 401);
+
+        $usuario = Usuario::where('correoelectronico', $credenciales['email'])->first();
+ 
+        if (!$usuario) return response()->json(['Usuario no autorizado'], 401);
+ 
+        if (!password_verify($credenciales['password'], $usuario->password)) {
+            return response()->json(['Password no autorizada'], 401);
+        }
+       
+        $usuario->token = bin2hex(random_bytes(32));
+        $usuario->save();
+
+        return response()->json([
+            'token' => $usuario->token
+        ], 200);
+        
     }
 
     public function logout(Request $request)
