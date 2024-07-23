a68bea44a1a57ed09a2c20e90abdbbf9883ea693
Lucas Naim Vitale <lucasnaimvitale04@gmail.com>
2024-07-23 16:50:26 +0200

    Termine Middleware de VerificacionToken

---
+++ Backend/API_Swaply/app/Http/Middleware/VerificacionToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Usuario;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerificacionToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+                $token = $request->header()['token'][0] ?? null;
+
+                if(!$token) return response()->json(['No autorizado'], 401);
+        
+                if(!Usuario::where('Token', $token)->first()){
+                    return response()->json(['No autorizado'], 401);
+                }
+        
+                return $next($request);
+    }
+}
