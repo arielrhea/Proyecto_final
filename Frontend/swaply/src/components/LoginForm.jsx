98a989aea6f69f23bef1912d5e5e33f133f5b79c
arielrhea <arielrhea@hotmail.com>
2024-07-24 19:46:59 +0200

    loginform comentado

--- Frontend/swaply/src/components/LoginForm.jsx
+++ Frontend/swaply/src/components/LoginForm.jsx
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
-import { useAuth } from '../context/AuthContext'; // Importa el hook de autenticación
-import './LoginForm.css'; // Agrega estilos según sea necesario
+// import React, { useState } from 'react';
+// // import { useAuth } from '../context/AuthContext'; // Importa el hook de autenticación
+// import './LoginForm.css'; // Agrega estilos según sea necesario
 
-const LoginPage = () => {
-    const [credentials, setCredentials] = useState({ email: '', password: '' });
-    const { authenticateUser } = useAuth(); // Usa la función de autenticación
+// const LoginPage = () => {
+//     const [credentials, setCredentials] = useState({ email: '', password: '' });
+//     const { authenticateUser } = useAuth(); // Usa la función de autenticación
 
-    const handleChange = (e) => {
-        setCredentials({
-            ...credentials,
-            [e.target.name]: e.target.value
-        });
-    };
+//     const handleChange = (e) => {
+//         setCredentials({
+//             ...credentials,
+//             [e.target.name]: e.target.value
+//         });
+//     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        authenticateUser(credentials);
-    };
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         authenticateUser(credentials);
+//     };
 
-    return (
-        <div className="login-page">
-            <h1>Iniciar sesión</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Correo Electrónico</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={credentials.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Contraseña</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={credentials.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Iniciar sesión</button>
-            </form>
-        </div>
-    );
-};
+//     return (
+//         <div className="login-page">
+//             <h1>Iniciar sesión</h1>
+//             <form onSubmit={handleSubmit}>
+//                 <div>
+//                     <label htmlFor="email">Correo Electrónico</label>
+//                     <input
+//                         type="email"
+//                         id="email"
+//                         name="email"
+//                         value={credentials.email}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <div>
+//                     <label htmlFor="password">Contraseña</label>
+//                     <input
+//                         type="password"
+//                         id="password"
+//                         name="password"
+//                         value={credentials.password}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <button type="submit">Iniciar sesión</button>
+//             </form>
+//         </div>
+//     );
+// };
 
-export default LoginPage;
+// export default LoginPage;
