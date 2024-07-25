31a04a3ac7573eaa3a41fe93b2bc47b8c3c060d7
AlopezR <adrialopezr@gmail.com>
2024-07-25 17:33:39 +0200

    searchbar funciona desde todas las vistas

--- Frontend/swaply/src/components/SearchBar.jsx
+++ Frontend/swaply/src/components/SearchBar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useContexto } from '../context/Context'; // Asegúrate de usar la ruta correcta
 import './SearchBar.css';
+import { buildQueries } from '@testing-library/react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
     const { busqueda, handleBusquedaChange } = useContexto();
+    const navigate = useNavigate();
+    
     const disablEnter=(event)=>{
         console.log(event)
         if(event.key=='Enter')
         {
-            console.log('eskere')
+            console.log({busqueda})
             event.preventDefault();
+            navigate('/', { state: { search: busqueda } }); // Redirige a la homepage con el valor de búsqueda
         }
     }
 
